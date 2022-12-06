b48f198024b5866983bd56deb3103bd72294e02c
Brad Ballard <brad@elmtechpros.com>
2022-12-06 08:45:24 -0500

    Fix Pundit deprecation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  include Pundit
+  include Pundit::Authorization
   include Pagy::Backend
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :company_branding
